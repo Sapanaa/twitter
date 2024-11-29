b1a52c1db2ee58f6d5f709a0f694166cef639a25
sapana <sapanadhami1111@gmail.com>
2024-11-29 08:27:29 +0000

    routes added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { v2 as cloudinary } from 'cloudinary';
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from './routes/auth.route.js'
 import connectDB from './db/connectDB.js'
 import cookieParser from 'cookie-parser'
 
-import userRoutes from './routes/user.routes.js'
-import postRoutes from './routes/post.routes.js'
+import userRoutes from './routes/user.route.js'
+import postRoutes from './routes/post.route.js'
 dotenv.config()
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
