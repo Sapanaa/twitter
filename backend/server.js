45427523ded1ce4290bf9a826843a0e300779666
sapana <sapanadhami1111@gmail.com>
2024-11-30 10:35:24 +0000

    production ready

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import authRoutes from './routes/auth.route.js'
 import connectDB from './db/connectDB.js'
 import cookieParser from 'cookie-parser'
-
+import path from 'path'
 import userRoutes from './routes/user.route.js'
 import postRoutes from './routes/post.route.js'
 import notificationRoutes from './routes/notification.route.js'
@@ -17,8 +17,10 @@ cloudinary.config({
 const app = express()
 
 const port = process.env.PORT || 5000
-
+const __dirname = path.resolve();
 //console.log("Mongo URI from .env:", process.env.MONGO_URI);
+
+
 app.use(express.json({limit: "5mb"}))  //to accept json req.body
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
@@ -26,6 +28,14 @@ app.use("/api/auth", authRoutes);  //localhost/api/auth
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
     connectDB()
